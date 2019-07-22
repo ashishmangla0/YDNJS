fcb070cf515491670d834ee8a39b9d1c7ec6ef08
ashishmangla0 <ashishmangla1991@gmail.com>
2019-07-22 16:07:11 +0530

    Update console-log.js

--- console-log.js
+++ console-log.js
@@ -1,2 +1,4 @@
 //this page have all the types of console methods
 //first method is "%c" which	Formats the output using the CSS style you provide
+
+console.log('%cThis is demo','color:red;font-size:40; background:green');
