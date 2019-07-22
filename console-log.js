ecd40e8b492d1b0c89f7119f2446f042a2b77d0e
ashishmangla0 <ashishmangla1991@gmail.com>
2019-07-22 16:33:30 +0530

    add new "%d" and "%i"

--- console-log.js
+++ console-log.js
@@ -2,3 +2,28 @@
 //first method is "%c" which	Formats the output using the CSS style you provide
 console.log('%cThis is demo','color:red;font-size:40px; background:green');
 console.log('%cThis text is styled!', 'color: #86CC00; background-color: blue; font-size: 20px; padding: 3px;')
+
+
+
+
+//second method is "%d" or "%i"	Integer
+var numberNew = 11;
+var secondNumber = 22;
+console.log('%i number %i this is demo',numberNew,secondNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+//print value
+var number = 11 * 9;
+var color = 'red';
+console.log(color,number);
+
